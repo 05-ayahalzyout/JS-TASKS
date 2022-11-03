e3dce62d6a9672c0f6cb93c8abf997377235d68b
05-ayahalzyout <115452706+05-ayahalzyout@users.noreply.github.com>
2022-11-03 16:26:58 +0200

    task

---
+++ task2/task2.js
@@ -0,0 +1,4 @@
+var t=50
+const b=3.14;
+let x=t*t*b;
+document.write(x);
---
+++ tasks3-11/task1/task.js
@@ -0,0 +1,22 @@
+var a=99;
+if (a>=0&&a<=49){
+document.write("fail");
+}
+else if(a>=50&&a<=59){
+document.write("D");
+}
+else if(a>=60&&a<=69){
+document.write("C")
+}
+else if(a>=70&&a<=79){
+document.write("B")
+}
+else if(a>=80&&a<=89){
+    document.write("A")
+    }
+    else if(a>=90&&a<=100){
+    document.write("+A")
+    }
+    else{
+        document.write("wrong")
+    }
